9c41526026464ffac9cee69e48feee4f92b2cd63
Ahmed Zedan <ahmedhamdizedan@gmail.com>
2025-04-22 22:07:55 +0200

    ggg

--- lib/whatsapp.ts
+++ lib/whatsapp.ts
@@ -5,22 +5,10 @@ let client: Client;
 
 // Initialize WhatsApp client
 export function getWhatsAppClient() {
-  if (!client) {
+   if (!client) {
     client = new Client({
       authStrategy: new LocalAuth(),
-      puppeteer: {
-        headless: true,
-        executablePath: "/usr/bin/chromium-browser",
-        args: [
-          "--no-sandbox",
-          "--disable-setuid-sandbox",
-          "--disable-gpu",
-          "--disable-dev-shm-usage",
-          "--disable-extensions",
-          "--disable-infobars",
-          "--window-size=1920,1080",
-        ],
-      },
+      puppeteer: { headless: true },
     });
 
     client.on("qr", (qr) => {
